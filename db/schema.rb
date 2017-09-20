2e381dac2916ea1d48ef5ff9a065bc9fd970367d
Masanobu Mizutani <James@MacBook-Air.local>
2017-09-20 23:55:44 +0900

    ユーザー情報登録導線の修正

--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,15 @@ ActiveRecord::Schema.define(version: 20170920121616) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.integer "status", limit: 1, default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
