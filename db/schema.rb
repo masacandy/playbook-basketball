4fce1fb0a244d5adda72ccd0f604227ca66b2648
Masanobu Mizutani <James@MacBook-Air.local>
2017-10-15 16:33:17 +0900

    余計なコミットの修正

--- db/schema.rb
+++ db/schema.rb
@@ -38,27 +38,4 @@ ActiveRecord::Schema.define(version: 20171015071827) do
     t.index ["sort"], name: "index_lessons_on_sort"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC" do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unconfirmed_email"], name: "index_users_on_unconfirmed_email"
-  end
-
 end
